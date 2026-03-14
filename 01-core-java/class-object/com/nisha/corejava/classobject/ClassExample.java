d8dd2063e55aeec5d7d207fcaa21fe46c13f9aea
Nisha Pinjar <nishapinjar09@gmail.com>
2026-03-14 07:25:14 -0700

    Added class and object example with package structure

---
+++ 01-core-java/class-object/com/nisha/corejava/classobject/ClassExample.java
@@ -0,0 +1,15 @@
+package com.nisha.corejava.classobject;
+
+public class ClassExample {//class keyword
+
+	int x = 100;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		ClassExample obj = new ClassExample(); // new keyword
+		//object is created
+		
+		System.out.println(obj.x);
+	}
+
+}
---
+++ 01-core-java/class-object/com/nisha/corejava/classobject/Example2.java
@@ -0,0 +1,17 @@
+package com.nisha.corejava.classobject;
+
+public class Example2 {
+	
+	int id;
+	String name;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Example2 obj = new Example2();
+		
+		obj.id = 2;
+		obj.name = "Nisha";
+		System.out.println(obj.id + " " + obj.name);
+	}
+
+}
