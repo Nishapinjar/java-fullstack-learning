e8df55a44ad501e51c321a6c49919a2d71eae8f2
Nisha Pinjar <nishapinjar09@gmail.com>
2026-04-08 16:30:47 -0700

    added filehandling code folder

---
+++ 02-oops/interfaces/com/nisha/oops/interfaces/Animal.java
@@ -0,0 +1,7 @@
+package com.nisha.oops.interfaces;
+
+public interface Animal {
+
+	void sound();
+
+}
---
+++ 02-oops/interfaces/com/nisha/oops/interfaces/Dog.java
@@ -0,0 +1,11 @@
+package com.nisha.oops.interfaces;
+
+public class Dog implements Animal {
+
+	@Override
+	public void sound() {
+		// TODO Auto-generated method stub
+		System.out.println("Dog barks");
+	}
+
+}
